f3c1d9e11a3e59337e89e76e11d7e1d245f1601d
jellybytes <simon.ruhnau@protonmail.com>
2024-06-20 10:54:26 +0000

    Changed prompt linux

--- linux/config-machine.sh
+++ linux/config-machine.sh
@@ -1,4 +1,4 @@
 # color prompt
 echo "" >> ~/.bashrc
-# original echo "export PS1=\"\[\e[32m\][\[\e[m\]\[\e[31m\]\u\[\e[m\]\[\e[32m\]@\[\e[m\]\[\e[35m\]\h\[\e[m\]:\[\e[34m\]\w\[\e[m\]\[\e[32m\]]\[\e[m\]\$ \""
-echo "export PS1=\"\[\e[32m\][\[\e[m\]\[\e[31m\]\u\[\e[m\]\[\e[32m\]@\[\e[m\]\[\e[35m\]\h\[\e[m\]:\[\e[34m\]\w\[\e[m\]\[\e[32m\]]\[\e[m\]\\$ \"" >> ~/.bashrc
+# export PS1="\[\e[33m\][\[\e[m\]\[\e[32m\]\u\[\e[m\]\[\e[33m\]@\[\e[m\]\[\e[32m\]\h\[\e[m\]:\[\e[34m\]\w\[\e[m\]\[\e[33m\]]\[\e[m\]\$ "
+echo "export PS1=\"\[\e[33m\][\[\e[m\]\[\e[32m\]\u\[\e[m\]\[\e[33m\]@\[\e[m\]\[\e[32m\]\h\[\e[m\]:\[\e[34m\]\w\[\e[m\]\[\e[33m\]]\[\e[m\]\\$ \"" >> ~/.bashrc
