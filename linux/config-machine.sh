0f6a769bb7cc290ef9e00fcf3ee8c150cbf5687f
jellybytes <simon.ruhnau@protonmail.com>
2024-06-20 10:16:04 +0000

    Linux color prompt

---
+++ linux/config-machine.sh
@@ -0,0 +1,4 @@
+# color prompt
+echo "" >> ~/.bashrc
+# original echo "export PS1=\"\[\e[32m\][\[\e[m\]\[\e[31m\]\u\[\e[m\]\[\e[32m\]@\[\e[m\]\[\e[35m\]\h\[\e[m\]:\[\e[34m\]\w\[\e[m\]\[\e[32m\]]\[\e[m\]\$ \""
+echo "export PS1=\"\[\e[32m\][\[\e[m\]\[\e[31m\]\u\[\e[m\]\[\e[32m\]@\[\e[m\]\[\e[35m\]\h\[\e[m\]:\[\e[34m\]\w\[\e[m\]\[\e[32m\]]\[\e[m\]\\$ \"" >> ~/.bashrc
