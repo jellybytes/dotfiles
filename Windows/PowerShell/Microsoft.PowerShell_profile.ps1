09991228f0e42d669dd749e5daa54e09842a1886
jellybytes <simon.ruhnau@protonmail.com>
2024-06-29 22:18:24 +0200

    Updated config

--- Windows/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Windows/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -14,7 +14,7 @@ $global:PSColor = @{
         Hidden     = @{ Color = 'DarkGray'; Pattern = '^\.' } 
         Code       = @{ Color = 'Magenta'; Pattern = '\.(java|c|cpp|cs|js|css|html)$' }
         Executable = @{ Color = 'Red'; Pattern = '\.(exe|bat|cmd|py|pl|ps1|psm1|vbs|rb|reg)$' }
-        Text       = @{ Color = 'Yellow'; Pattern = '\.(txt|cfg|conf|ini|csv|log|config|xml|yml|md|markdown)$' }
+        Text       = @{ Color = 'Red'; Pattern = '\.(txt|cfg|conf|ini|csv|log|config|xml|yml|md|markdown)$' }
         Compressed = @{ Color = 'Green'; Pattern = '\.(zip|tar|gz|rar|jar|war)$' }
     }
     Service = @{
