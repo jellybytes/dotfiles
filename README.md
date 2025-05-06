7ebcfbbcf045326e47b037c2704cf49540ab9b7f
Simon Ruhnau <ruhnau@raceresult.com>
2025-05-06 21:38:26 +0200

    removed old configs

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
-# Linux configs
+# dotfiles jellybytes
+
+## Linux configs
 Run `bash dotfiles/linux/config-machine.sh`.
 
-# Windows configs
-In order to run scripts run: `PS C:\> Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser`
-
-To apply configs run the script: `PS C:\> dotfiles/windows/Config-Windos.ps1 set-config`. 
-
-If configs on local machine changed run: `PS C:\> dotfiles/windows/Config-Windos.ps1 set-config` and commit changes.
+## Windows configs
+Run the script: `PS C:\> dotfiles/windows/Config-Windos.ps1 set`. 
+If configs on local machine changed run: `PS C:\> dotfiles/windows/Config-Windos.ps1 get` and commit changes.
--- Windows/Config-Windows.ps1
+++ Windows/Config-Windows.ps1
@@ -1,29 +1,41 @@
 # params:
-#   config -> apply configes file into filesystem
-#   get-config -> get current configured 
-
-# NOTE: need to create folders before Copy-Item -> else Could not fid part of path..
+#   set -> apply configes file into filesystem
+#   get -> get current config files
 
 $param=$args[0]
-if ($param -eq "set-config") {
-	# Visual Code Editor
-	Copy-Item .\vscode\settings.json  ~\AppData\Roaming\Code\User\settings.json -Force
-	Copy-Item .\vscode\keybindings.json  ~\AppData\Roaming\Code\User\keybindings.json -Force
-	Write-Output "VSCode configured!"
-
-	# Power Shell
-	Copy-Item .\PowerShell\Microsoft.PowerShell_profile.ps1  ~\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1 -Force
-	Copy-Item .\PowerShell\Microsoft.PowerShell_profile.ps1  ~\Documents\WindowsPowerShell\Microsoft.VSCode_profile.ps1 -Force
+if ($param -eq "set") {
+	# vscode
+	# Copy-Item .\vscode\settings.json  ~\AppData\Roaming\Code\User\settings.json -Force
+	# Copy-Item .\vscode\keybindings.json  ~\AppData\Roaming\Code\User\keybindings.json -Force
+	# PowerShell
+	Copy-Item .\ps\Microsoft.PowerShell_profile.ps1  ~\Documents\PowerShell\Microsoft.PowerShell_profile.ps1 -Force
+	Copy-Item .\ps\Microsoft.PowerShell_profile.ps1  ~\Documents\PowerShell\Microsoft.VSCode_profile.ps1 -Force
 	Write-Output "PowerShell Profile configured!"
-}
-elseif ($param -eq "get-config") {
-	# Visual Code Editor
-	Copy-Item ~\AppData\Roaming\Code\User\settings.json .\vscode\settings.json -Force
-	Copy-Item ~\AppData\Roaming\Code\User\keybindings.json .\vscode\keybindings.json -Force
-	# Power Shell
-	Copy-Item ~\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1 .\PowerShell\Microsoft.PowerShell_profile.ps1   -Force
+	# glazeWM
+	Copy-Item .\glazewm\config.yaml ~\.config\glazewm\config.yaml -Force
+	# superfile
+	Copy-Item .\superfile\config.toml ~\.config\superfile\config.toml -Force
+	# yasb
+	Copy-Item .\yasb\config.yaml ~\.config\yasb\config.yaml -Force
+	Copy-Item .\yasb\styles.css ~\.config\yasb\styles.css -Force
 
-	Write-Output "PowerShell Profile copied!"
+	Write-Output "machine configured!"
+}
+elseif ($param -eq "get") {
+	# Copy-Item ~\AppData\Roaming\Code\User\settings.json .\vscode\settings.json -Force
+	# Copy-Item ~\AppData\Roaming\Code\User\keybindings.json .\vscode\keybindings.json -Force
+	# Power Shell
+	Copy-Item ~\Documents\PowerShell\Microsoft.PowerShell_profile.ps1 .\ps\Microsoft.PowerShell_profile.ps1 -Force
+	Copy-Item ~\Documents\PowerShell\Microsoft.VSCode_profile.ps1 .\ps\Microsoft.VSCode_profile.ps1 -Force
+	# glazeWM
+	Copy-Item ~\.config\glazewm\config.yaml .\glazewm\config.yaml -Force
+	# superfile
+	Copy-Item ~\.config\superfile\config.toml .\superfile\config.toml -Force
+	# yasb
+	Copy-Item ~\.config\yasb\config.yaml .\yasb\config.yaml -Force
+	Copy-Item ~\.config\yasb\styles.css .\yasb\styles.css -Force
+
+	Write-Output "all configs copied!"
 }
 else {
 	Write-Output "Error: Command not found"
