c17e8f069d2a35ed9b50cc0841bc1166938d79ea
Simon Ruhnau <ruhnau@raceresult.com>
2025-05-06 21:37:58 +0200

    switched to glazeWM

---
+++ Windows/ps/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,18 @@
+# neovim config path
+$env:XDG_CONFIG_HOME = "$Home\.config\"
+Set-Alias spf $env:UserProfile\.config\superfile\run.bat
+
+# enter %PSStyle to see all colors
+$PSStyle.FileInfo.Directory = $PSStyle.Foreground.Blue
+
+$env:EDITOR = "code"
+
+function prompt {
+	write-host "[" -ForegroundColor Gray -NoNewline
+	write-host "$env:UserName" -ForegroundColor Cyan -NoNewline
+	write-host "@" -ForegroundColor Green -NoNewline
+	write-host "$env:COMPUTERNAME" -ForegroundColor Cyan  -NoNewline
+	write-host "] " -ForegroundColor Gray -NoNewline
+	write-host $ExecutionContext.SessionState.Path.CurrentLocation
+	return "> "
+}
