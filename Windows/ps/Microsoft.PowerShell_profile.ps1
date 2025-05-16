85ba50be899fa1020c6873f64bcde7636f413d04
Simon Ruhnau <ruhnau@raceresult.com>
2025-05-16 08:03:41 +0200

    updated PowerShell prompt

--- Windows/ps/Microsoft.PowerShell_profile.ps1
+++ Windows/ps/Microsoft.PowerShell_profile.ps1
@@ -5,14 +5,15 @@ Set-Alias spf $env:UserProfile\.config\superfile\run.bat
 # enter %PSStyle to see all colors
 $PSStyle.FileInfo.Directory = $PSStyle.Foreground.Blue
 
+Set-PSReadLineOption -EditMode Vi
+Set-PSReadLineKeyHandler -Chord Tab -Function Complete
+
 $env:EDITOR = "code"
 
 function prompt {
-	write-host "" -ForegroundColor Gray -NoNewline
+	write-host "[" -ForegroundColor Gray -NoNewline
 	write-host "$env:UserName" -ForegroundColor Cyan -NoNewline
-	write-host "@" -ForegroundColor Green -NoNewline
-	write-host "$env:COMPUTERNAME" -ForegroundColor Cyan  -NoNewline
-	write-host " " -ForegroundColor Gray -NoNewline
-	write-host $ExecutionContext.SessionState.Path.CurrentLocation -NoNewline
-	return "\> "
+	write-host "] " -ForegroundColor Gray -NoNewline
+	write-host $ExecutionContext.SessionState.Path.CurrentLocation -ForegroundColor Magenta
+	return "> "
 }
