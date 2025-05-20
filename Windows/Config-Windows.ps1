01194ea2a9a883ff066058c341ccbcb8aeb5a10f
Simon Ruhnau <ruhnau@raceresult.com>
2025-05-20 20:42:16 +0200

    added powershell aliases and changed prompt

--- Windows/Config-Windows.ps1
+++ Windows/Config-Windows.ps1
@@ -9,7 +9,6 @@ if ($param -eq "set") {
 	# Copy-Item .\vscode\keybindings.json  ~\AppData\Roaming\Code\User\keybindings.json -Force
 	# PowerShell
 	Copy-Item .\ps\Microsoft.PowerShell_profile.ps1  ~\Documents\PowerShell\Microsoft.PowerShell_profile.ps1 -Force
-	Copy-Item .\ps\Microsoft.PowerShell_profile.ps1  ~\Documents\PowerShell\Microsoft.VSCode_profile.ps1 -Force
 	# glazeWM
 	Copy-Item .\glazewm\config.yaml ~\.config\glazewm\config.yaml -Force
 	# superfile
@@ -25,11 +24,8 @@ if ($param -eq "set") {
 	Write-Output "machine configured!"
 }
 elseif ($param -eq "get") {
-	# Copy-Item ~\AppData\Roaming\Code\User\settings.json .\vscode\settings.json -Force
-	# Copy-Item ~\AppData\Roaming\Code\User\keybindings.json .\vscode\keybindings.json -Force
 	# Power Shell
 	Copy-Item ~\Documents\PowerShell\Microsoft.PowerShell_profile.ps1 .\ps\Microsoft.PowerShell_profile.ps1 -Force
-	Copy-Item ~\Documents\PowerShell\Microsoft.VSCode_profile.ps1 .\ps\Microsoft.VSCode_profile.ps1 -Force
 	# glazeWM
 	Copy-Item ~\.config\glazewm\config.yaml .\glazewm\config.yaml -Force
 	# superfile
--- Windows/ps/Microsoft.PowerShell_profile.ps1
+++ Windows/ps/Microsoft.PowerShell_profile.ps1
@@ -1,23 +1,40 @@
-# neovim config path
+Invoke-Expression (&starship init powershell)
+
+# Environment variables
 $env:EDITOR = "code"
 $env:STARSHIP_CONFIG = "$HOME\.config\starship\starship.toml"
 $env:XDG_CONFIG_HOME = "$Home\.config\"
 
+# Aliases
 Set-Alias spf $env:UserProfile\.config\superfile\run.bat
+Set-Alias conf $env:UserProfile\.config\superfile\run.bat
+
+Function fConf {
+	cd ~\.config\
+	code .
+	exit
+}
+Set-Alias conf fConf
+Function fCdRepos {
+	cd ~\Documents\repos\
+}
+Set-Alias cdr fCdRepos
+
+Function fCdReposWSL {
+	wsl --cd ~/repos
+}
+Set-Alias cdrw fCdReposWSL
+
+Function fVSCodeHere {
+	code .
+	exit
+}
+Set-Alias vsc fVSCodeHere
 
 # enter %PSStyle to see all colors
 $PSStyle.FileInfo.Directory = $PSStyle.Foreground.Blue
 
 Set-PSReadLineOption -EditMode Vi
-Set-PSReadLineKeyHandler -Chord Tab -Function Complete
-
-
-Invoke-Expression (&starship init powershell)
-
-# function prompt {
-# 	write-host "[" -ForegroundColor Gray -NoNewline
-# 	write-host "$env:UserName" -ForegroundColor Cyan -NoNewline
-# 	write-host "] " -ForegroundColor Gray -NoNewline
-# 	write-host $ExecutionContext.SessionState.Path.CurrentLocation -ForegroundColor Magenta
-# 	return "> "
-# }
+# Set-PSReadLineKeyHandler -Chord Tab -Function Complete
+Set-PSReadLineKeyHandler -Chord "Tab" -Function ForwardWord
+Set-PSReadlineOption -BellStyle None
